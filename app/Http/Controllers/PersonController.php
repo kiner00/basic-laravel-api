2db042764fd7b1d933a3296c8a6fa17b315d2e0e
Kiner Mercurio <kiner.mercurio@xurpas.com>
2019-08-29 15:09:23 +0800

    added store and update to database

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -18,8 +18,37 @@ class PersonController extends Controller
         return new PersonResource($person);
     }
 
+    /**
+     * @return PersonResourceCollection
+     */
     public function index(): PersonResourceCollection
     {
         return new PersonResourceCollection(Person::paginate());
+    }
+
+    /**
+     * @param Request $request
+     * @return PersonResource
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name'    =>  'required',
+            'last_name'    =>  'required',
+            'email'    =>  'required',
+            'phone'    =>  'required',
+            'city'    =>  'required',
+        ]);
+
+        $person = Person::create($request->all());
+
+        return new PersonResource($person);
+    }
+
+    public function update(Person $person, Request $request): PersonResource
+    {
+        $person->update($request->all());
+
+        return new PersonResource($person);
     }
 }
